c94eecc4dcec989a6e11f7bd37d460fd10f9ea1d
Thotakura Abhishek Paul <tabhishekpaul@Thotakuras-MacBook-Pro.local>
2025-10-22 18:52:42 +0530

    idle devices fixes

--- tracker_v2/main.go
+++ tracker_v2/main.go
@@ -1708,7 +1708,7 @@ func RunDeviceTracker(runSteps []int) error {
 	}
 
 	if step4 {
-		consumerFolder := filepath.Join(outputFolder, "consumers_raw")
+		consumerFolder := filepath.Join(outputFolder, "consumer_raw")
 		idleDevicesPath := filepath.Join(outputFolder, "Idle_devices.json")
 
 		matcher := NewConsumerDeviceMatcher(outputFolder, consumerFolder, idleDevicesPath)
